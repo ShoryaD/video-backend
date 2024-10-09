af93d47e2999c53432f9de5896f0d18bf805fe33
ShoryaD <shoryadubey7610@gmail.com>
2024-10-10 00:00:42 +0530

    cloudinary and multer setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+dotenv.config({
+    path: './env'
+})
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        // Upload the file on Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+        })
+        // File has been Uploaded successfully
+        console.log("File Uploaded", response.url);
+        return response
+        
+    } catch (error) {
+        fs.unlinkSynv(localFilePath) // Remove the locally save dtemporary file as the upload operation got failed
+        return null;
+    }
+}
+
+export { uploadOnCloudinary }
