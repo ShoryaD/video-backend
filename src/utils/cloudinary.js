03282b660356f1ddd0ec5c38f6e3fe340f2753c9
ShoryaD <shoryadubey7610@gmail.com>
2024-10-11 20:43:41 +0530

    user created done successfully

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,32 +1,41 @@
-import dotenv from 'dotenv';
-import { v2 as cloudinary } from 'cloudinary';
-import fs from 'fs';
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
 
-dotenv.config({
-    path: './env'
-})
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null
-        // Upload the file on Cloudinary
+        //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: 'auto',
+            resource_type: "auto"
         })
-        // File has been Uploaded successfully
-        console.log("File Uploaded", response.url);
-        return response
-        
-    } catch (error) {
-        fs.unlinkSynv(localFilePath) // Remove the locally save dtemporary file as the upload operation got failed
-        return null;
+
+        // Check if the file exists before deleting it
+      if (fs.existsSync(localFilePath)) {
+        fs.unlinkSync(localFilePath);  // Delete the file only if it exists
+        // console.log('File deleted successfully from local storage');
+    } else {
+        console.error('File not found:', localFilePath);
+    }
+
+    return response;
+
+} catch (error) {
+    // In case of error, also check before attempting to delete the file
+    if (fs.existsSync(localFilePath)) {
+        fs.unlinkSync(localFilePath); // Delete the file if it exists
+    }
+    // console.error('Error uploading to Cloudinary or deleting local file:', error);
+    return null;
     }
 }
 
-export { uploadOnCloudinary }
+
+
+export {uploadOnCloudinary}
